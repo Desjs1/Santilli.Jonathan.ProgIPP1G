f564ccb29fcf92f7e99cf12dae80daeae1469cd9
jonathan santilli <60760258+Desjs1@users.noreply.github.com>
2020-10-19 19:33:56 -0300

    Add files via upload
    parcial programacion 1g

---
+++ parcial_programacion/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Primer ejercicio
+/*#define PERSONAS 3
+void actualizarMuertos (int recuperados);
+int main()
+{
+    int id [PERSONAS] = {1,2,3};
+    char nombres [3] [52]= {"Angelo","Maria","Tomas"};
+    int infectados = 500;
+    int recuperados = 500;
+    int muertos = 1000;
+    actualizarMuertos (recuperados);
+    return 0;
+}
+void actualizarMuertos (int recuperados){
+for(int i = 0;i<PERSONAS;i++){
+    recuperados++;
+}
+printf("recuperados en el dia: %d",recuperados);
+}
+*/
+//Tercer ejercicio
+/*
+#define NOMBRE 52
+#define ASC 1
+void ordenarCadena(char cadena[],int tam, int criterio);
+int main(){
+    char cadenachar[NOMBRE] = {"caracter"};
+    ordenarCadena(cadena,NOMBRE,ASC);
+    return 0;
+}
+void ordenarCadena(char cadena[],int tam, int criterio){
+    char aux ;
+for(int i = 0;i<tam;i++){
+        for(int j = i+1;j<tam;j++){
+            if(strcmp(cadena[i],cadena[j])!=0 && criterio){
+                aux = cadena[i];
+                cadena[i] = cadena[j];
+                cadena[j] = aux;
+            }
+        }
+        printf("%s",&cadena);
+}
+}
+*/
